259151fa77c62e2d8c22e33158dfc1057acdc5f0
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-11-16 18:30:09 +0100

    fix: remove unused optimistic update

--- src/hooks/mutations/use-create-calendar-event.ts
+++ src/hooks/mutations/use-create-calendar-event.ts
@@ -1,7 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { endOfDay, startOfDay } from 'date-fns';
 
 import { createCalendarEvent } from '@/services/calendar.service';
-import { CalendarDayAvailability, CalendarEventItem } from '@/types/calendar.types';
+import { CalendarEventItem } from '@/types/calendar.types';
 
 interface CreateCalendarEventParams {
   summary: string;
@@ -15,40 +16,16 @@ export function useCreateCalendarEvent() {
   return useMutation<CalendarEventItem, Error, CreateCalendarEventParams>({
     mutationFn: ({ summary, calendarId, startDate, endDate }) =>
       createCalendarEvent(calendarId, summary, startDate, endDate),
-    onMutate: async ({ calendarId, startDate, endDate }) => {
-      const queryKey = ['calendar-availability', startDate.toISOString(), endDate.toISOString()];
-      await queryClient.cancelQueries({ queryKey });
-      const previousAvailability = queryClient.getQueryData<CalendarDayAvailability[]>(queryKey);
-      queryClient.setQueryData<CalendarDayAvailability[]>(queryKey, (old) => {
-        if (!old) return old;
-        return old.map((availability) => {
-          if (availability.calendarId === calendarId) {
-            return {
-              ...availability,
-              fullPeriodAvailable: false,
-              availability: [
-                ...availability.availability,
-                {
-                  dateFrom: startDate,
-                  dateTo: endDate,
-                  available: false,
-                  isOwn: true,
-                },
-              ],
-            };
-          }
-          return availability;
-        });
-      });
-
-      return previousAvailability;
-    },
-    onSuccess: (data, variables) => {
-      queryClient.invalidateQueries({
-        queryKey: ['calendar-availability', variables.startDate.toISOString(), variables.endDate.toISOString()],
+    onSuccess: async (_, variables) => {
+      await queryClient.invalidateQueries({
+        queryKey: [
+          'calendar-availability',
+          startOfDay(variables.startDate).toISOString(),
+          endOfDay(variables.endDate).toISOString(),
+        ],
       });
     },
-    onError: (err, variables, context) => {
+    onError: (_, variables, context) => {
       if (context) {
         queryClient.setQueryData(['calendar-availability', variables.startDate, variables.endDate], context);
       }
